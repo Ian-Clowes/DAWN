89001dc9530443b79a254312c2df34d737c0b755
Ian-Clowes <65678428+Ian-Clowes@users.noreply.github.com>
2020-06-03 01:04:42 +0100

    test_storage: added ability to read script file

--- src/test/test_storage.c
+++ src/test/test_storage.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "dawn_iwinfo.h"
 #include "utils.h"
@@ -111,7 +112,7 @@ int ap_array_helper_auto(int action, int i0, int i1)
         }
         break;
     default:
-        ret = 1;
+        ret = -1;
         break;
     }
 
@@ -149,7 +150,7 @@ int client_array_helper_auto(int action, int i0, int i1)
         }
         break;
     default:
-        ret = 1;
+        ret = -1;
         break;
     }
 
@@ -187,7 +188,7 @@ int auth_entry_array_helper_auto(int action, int i0, int i1)
         }
         break;
     default:
-        ret = 1;
+        ret = -1;
         break;
     }
 
@@ -225,66 +226,54 @@ int probe_array_helper_auto(int action, int i0, int i1)
         }
         break;
     default:
-        ret = 1;
+        ret = -1;
         break;
     }
 
     return ret;
 }
 
-int main(int argc, char** argv)
+
+int consume_actions(int argc, char* argv[]);
+
+int consume_actions(int argc, char* argv[])
 {
-int ret = 0;
-int args_ok = 1;
-int args_required = 0; // Suppress compiler warming by assigning initial value
+    int ret = 0;
+    int args_required = 0; // Suppress compiler warming by assigning initial value
 
-    printf("DAWN datastorage.c test harness.  Ready for commands...\n");
+    int curr_arg = 0;
 
-    /* Step past command name on args, ie argv[0]*/
-    int curr_arg = 1;
-    argv++;
-
-    while (curr_arg < argc && args_ok)
+    while (curr_arg < argc && ret == 0)
     {
-        if (strcmp(*argv, "help") == 0)
+        if (strcmp(*argv, "probe_sort") == 0)
         {
-            args_required = 1;
-	        if (curr_arg + args_required > argc) goto next_command;
-
-	        printf("Help is on its way...\n");
+            args_required = 2;
+            if (curr_arg + args_required <= argc)
+            {
+                sort_string = argv[1];
+            }
         }
         else if (strcmp(*argv, "ap_show") == 0)
         {
             args_required = 1;
-            if (curr_arg + args_required > argc) goto next_command;
 
             print_ap_array();
         }
         else if (strcmp(*argv, "probe_show") == 0)
         {
             args_required = 1;
-            if (curr_arg + args_required > argc) goto next_command;
 
             print_probe_array();
-        }
-        else if (strcmp(*argv, "probe_sort") == 0)
-        {
-            args_required = 2;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            sort_string = argv[1];
         }
         else if (strcmp(*argv, "client_show") == 0)
         {
             args_required = 1;
-            if (curr_arg + args_required > argc) goto next_command;
 
             print_client_array();
         }
         else if (strcmp(*argv, "auth_entry_show") == 0)
         {
             args_required = 1;
-            if (curr_arg + args_required > argc) goto next_command;
 
             printf("--------APs------\n");
             for (int i = 0; i <= denied_req_last; i++) {
@@ -295,68 +284,75 @@ int args_required = 0; // Suppress compiler warming by assigning initial value
         else if (strcmp(*argv, "ap_add_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            ap_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = ap_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "ap_del_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            ap_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = ap_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "probe_add_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            probe_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = probe_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "probe_del_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            probe_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = probe_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "client_add_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            client_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = client_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "client_del_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            client_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = client_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "auth_entry_add_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            auth_entry_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = auth_entry_array_helper_auto(0, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else if (strcmp(*argv, "auth_entry_del_auto") == 0)
         {
             args_required = 3;
-            if (curr_arg + args_required > argc) goto next_command;
-
-            auth_entry_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            if (curr_arg + args_required <= argc)
+            {
+                ret = auth_entry_array_helper_auto(1, atoi(*(argv + 1)), atoi(*(argv + 2)));
+            }
         }
         else
 	    {
             args_required = 1;
-	        if (curr_arg + args_required > argc) goto next_command;
 
-	        printf("COMMAND \"%s\": Unknown - skipping!\n", *argv);
+	        printf("COMMAND \"%s\": Unknown - stopping!\n", *argv);
+            ret = -1;
 	    }
 
-next_command:
 	    curr_arg += args_required;
         if (curr_arg <= argc)
         {
@@ -367,11 +363,154 @@ next_command:
         {
             // There aren't enough args left to give the parameters of the current action
             printf("Commands are mangled at: \"%s\"!\n", *argv);
-            args_ok = 0;
+            ret = -1;
         }
     }
 
-    printf("\n\nDAWN datastorage.c test harness - finshed.  \n");
+    return ret;
+}
+
+int process_script_line(char* line, size_t len);
+#define MAX_LINE_ARGS 5
+int process_script_line(char* line, size_t len)
+{
+    int argc = 0;
+    char* argv[MAX_LINE_ARGS];
+    bool in_white = true;
+    bool force_blank = false;
+    
+    int ret = 0;
+
+    //printf("%lu: \"%s\"\n", len, line);
+    while (len > 0 && !ret)
+    {
+        if (isblank(*line) || (*line == '\n') || (*line == '\r') || (*line == '#') || force_blank)
+        {
+            if (*line == '#')
+            {
+                //printf("Blanking 0x%02X...\n", *line);
+                force_blank = true;
+            }
+
+            //printf("Zapping 0x%02X...\n", *line);
+            *line = '\0';
+            in_white = true;
+        }
+        else
+        {
+            if (in_white)
+            {
+                //printf("Marking 0x%02X...\n", *line);
+                if (argc == MAX_LINE_ARGS)
+                {
+                    printf("ERROR: Script line exceeds permitted arg count!\n");
+                    ret = -1;
+                }
+                else
+                {
+                    argv[argc] = line;
+                    argc++;
+
+                    in_white = false;
+                }
+            }
+        }
+
+        len--;
+        line++;
+    }
+
+    if (!ret)
+        ret = consume_actions(argc, argv);
+
+    return ret;
+}
+
+int main(int argc, char* argv[])
+{
+    FILE* fp;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int ret = 0;
+
+    printf("DAWN datastorage.c test harness...\n\n");
+
+    if ((argc == 1) || !strcmp(*(argv + 1), "help") || !strcmp(*(argv + 1), "--help") || !strcmp(*(argv + 1), "-h"))
+    {
+        printf("Usage: %s [commands]\n\n", *argv);
+        printf("  [action [arg...]]... : Read test actions from command line\n");
+        printf("    --script [file]... : Read test script from file(s) (NB: \"-\" is a valid name indicating STDIN) {-s}\n");
+        printf("                     - : Read test script from STDIN (and remaining arguments as script file names)\n");
+        printf("                --help : This help message {-h, help}\n");
+        printf("NB: Contents of {braces} indicate equivalent command\n");
+    }
+    else
+    {
+        // Step past command name on args, ie argv[0]
+        argc--;
+        argv++;
+
+        if (!strcmp(*argv, "--script") || !strcmp(*argv, "-s") || !strcmp(*argv, "-"))
+        {
+            if (!strcmp(*argv, "--script") || !strcmp(*argv, "-s"))
+            {
+                argc--;
+                argv++;
+            }
+
+            // Read script from file[s]
+            while (argc > 0 && ret == 0)
+            {
+                if (!strcmp(*argv, "-"))
+                {
+                    fp = stdin;
+                    printf("Consuming script from STDIN\n");
+                }
+                else
+                {
+                    fp = fopen(*argv, "r");
+                    if (fp == NULL)
+                    {
+                        printf("Error opening script file: %s\n", *argv);
+                        ret = -1;
+                    }
+                    else
+                    {
+                        printf("Consuming script file: %s\n", *argv);
+                    }
+                }
+
+                if (ret == 0)
+                {
+                    read = getline(&line, &len, fp);
+                    while (!ret && read != -1)
+                    {
+                        printf("Processing: %s\n", line);
+                        ret = process_script_line(line, read);
+                        if (!ret)
+                            read = getline(&line, &len, fp);
+                    }
+
+                    if (fp != stdin)
+                        fclose(fp);
+
+                    if (line)
+                        free(line);
+                }
+
+                argc--;
+                argv++;
+            }
+        }
+        else
+        {
+            // Assume direct input on command line
+            ret = consume_actions(argc, argv);
+        }
+    }
+    printf("\nDAWN datastorage.c test harness - finshed.  \n");
 
     return ret;
 }
